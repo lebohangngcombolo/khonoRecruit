1c7431347bb3f9866f7a17764be6b44d871f270b
lebohangngcombolo <lebohangngcombolo@gmail.com>
2025-11-04 12:28:12 +0200

    render

--- server/run.py
+++ server/run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.extensions import db
+import os
 
 app = create_app()
 
@@ -7,4 +8,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Use the port Render provides, or default to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
